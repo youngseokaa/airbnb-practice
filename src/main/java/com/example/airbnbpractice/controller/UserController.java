1c092061176304e22393523dd099590abd9fde6d
kikidy12 <kikidy12@naver.com>
2023-03-03 21:09:05 +0900

    환경변수들 분리

--- src/main/java/com/example/airbnbpractice/controller/UserController.java
+++ src/main/java/com/example/airbnbpractice/controller/UserController.java
@@ -1,4 +1,12 @@
 package com.example.airbnbpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
--- src/main/java/com/example/airbnbpractice/entity/Users.java
+++ src/main/java/com/example/airbnbpractice/entity/Users.java
@@ -1,4 +1,19 @@
 package com.example.airbnbpractice.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
