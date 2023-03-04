9e389d75b0e191c62c11f7dc5e0896acf59bad8a
kikidy12 <kikidy12@naver.com>
2023-03-04 13:18:47 +0900

    FIX : signup 수정 회원가입 Save누락된 부분 수정 중복회원 체크 dto에 getter추가

--- src/main/java/com/example/airbnbpractice/common/dto/ErrorMessage.java
+++ src/main/java/com/example/airbnbpractice/common/dto/ErrorMessage.java
@@ -11,6 +11,7 @@ public enum ErrorMessage {
     PATTERN("잘못된 양식의 값이 있습니다."),
     TOKEN_ERROR("토큰이 유효하지 않습니다."),
     S3_UPLOAD_ERROR("이미지 업로드 실패"),
+    DUPLE_USER("이미 가입된 유저가 있습니다."),
     TOKEN_EXPIRED("토큰이 만료되었습니다.");
 
 
