9befeea6817d32488513317f40b2fe749f72521d
jenny <jennykim6890@naver.com>
2023-03-06 21:19:48 +0900

    feat : tagtype 이름변경

--- src/main/java/com/example/airbnbpractice/controller/TagTypeController.java
+++ src/main/java/com/example/airbnbpractice/controller/TagTypeController.java
@@ -4,7 +4,7 @@ import com.example.airbnbpractice.common.dto.ResponseDto;
 import com.example.airbnbpractice.common.security.UserDetailsImpl;
 import com.example.airbnbpractice.dto.*;
 import com.example.airbnbpractice.entity.UserRoleEnum;
-import com.example.airbnbpractice.service.TagService;
+import com.example.airbnbpractice.service.TagTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
@@ -16,33 +16,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tag")
 @RequiredArgsConstructor
-public class TagController {
+public class TagTypeController {
 
-    private final TagService tagService;
+    private final TagTypeService tagTypeService;
 
     @PostMapping("/tagtype")
     @Secured(UserRoleEnum.Authority.ADMIN)
     public ResponseDto<TagTypeResponseDto> addTagType(@RequestBody TagTypeRequestDto tagTypeRequestDto){
-        TagTypeResponseDto trd = tagService.addTagType(tagTypeRequestDto);
+        TagTypeResponseDto trd = tagTypeService.addTagType(tagTypeRequestDto);
         return ResponseDto.of(HttpStatus.OK,"태그 타입 등록에 성공하였습니다",trd);
     }
 
     @GetMapping("/tagtype")
     public ResponseDto<List<TagTypeReadDto>> readTagType(@AuthenticationPrincipal UserDetailsImpl userDetails){
-        TagTypeResponseDto trd = tagService.readTagType();
+        TagTypeResponseDto trd = tagTypeService.readTagType();
     }
 
     @PutMapping("/tagtype/{tagtypeId}")
     @Secured(UserRoleEnum.Authority.ADMIN)
     ResponseDto<TagTypeResponseDto> putTagType(@PathVariable Long tagTypeId,@RequestBody TagTypeRequestDto tagTypeRequestDto){
-        TagTypeResponseDto trd = tagService.putTagType(tagTypeRequestDto,tagTypeId);
+        TagTypeResponseDto trd = tagTypeService.putTagType(tagTypeRequestDto,tagTypeId);
         return ResponseDto.of(HttpStatus.OK,"태그 타입 수정에 성공하였습니다",trd);
     }
 
     @DeleteMapping("/tagtype/{tagtypeId}")
     @Secured(UserRoleEnum.Authority.ADMIN)
     ResponseDto deleteTagType(@PathVariable Long tagTypeId){
-        tagService.deleteTagType(tagTypeId);
+        tagTypeService.deleteTagType(tagTypeId);
         return ResponseDto.of(HttpStatus.OK, "삭제 성공");
     }
 }
--- src/main/java/com/example/airbnbpractice/service/TagTypeService.java
+++ src/main/java/com/example/airbnbpractice/service/TagTypeService.java
@@ -2,30 +2,23 @@ package com.example.airbnbpractice.service;
 
 import com.example.airbnbpractice.common.CustomClientException;
 import com.example.airbnbpractice.common.dto.ErrorMessage;
-import com.example.airbnbpractice.common.security.UserDetailsImpl;
 import com.example.airbnbpractice.dto.TagTypeReadDto;
 import com.example.airbnbpractice.dto.TagTypeRequestDto;
 import com.example.airbnbpractice.dto.TagTypeResponseDto;
-import com.example.airbnbpractice.entity.Tag;
 import com.example.airbnbpractice.entity.TagType;
-import com.example.airbnbpractice.entity.User;
 import com.example.airbnbpractice.repository.TagRepository;
 import com.example.airbnbpractice.repository.TagTypeRepository;
 import com.example.airbnbpractice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-
-import static com.example.airbnbpractice.common.dto.ErrorMessage.NO_USER;
 
 @Service
 @RequiredArgsConstructor
-public class TagService {
+public class TagTypeService {
 
     private final TagTypeRepository tagTypeRepository;
     private final UserRepository userRepository;
